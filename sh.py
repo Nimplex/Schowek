02cac1775b4f679ebe297cf0977fc01801ac2974
Nimplex <39964594+Nimplex@users.noreply.github.com>
2019-12-06 23:21:18 +0100

    Update sh.py

--- sh.py
+++ sh.py
@@ -1,40 +1,66 @@
 from tkinter import *
 import pyperclip
-rows = 1
-column = 0
-#Window
+
 window = Tk()
 window.title("Schowek")
-window.geometry('700x200')
+window.resizable(width=False, height=False)
 
-#Clicked function
+def all_children (window) :
+    _list = window.winfo_children()
+
+    for item in _list :
+        if item.winfo_children() :
+            _list.extend(item.winfo_children())
+
+    return _list
+
+def clear():
+    widget_list = all_children(window)
+    for item in widget_list:
+        item.pack_forget()
+    run()
+
 def clicked(text):
     pyperclip.copy(text)
     pyperclip.paste()
 
-#Add function
+frame = Frame(window)
 def add():
-    global rows , column
-    for i in range(rows):
-        btn = Button(window, text=txt.get())
-        btn.configure(command=lambda j=i+1: clicked(btn.cget("text")))
-        btn.grid(column=column, row=rows)
-    rows += 1
-    if rows == 5:
-        rows = 0
-        column += 1
+    for i in range(1):
+        btn = Button(window, text=txt.get(), wraplength=80, justify=LEFT , command=lambda j=i+1: clicked(btn.cget("text")))
+        btn.pack(side=BOTTOM, expand=True)
 
-#Text
+def run():
+    lbl = Label(window, text="Witaj w schowku!")
+    lbl.pack()
+
+    txt = Entry(window, width=50)
+    txt.pack()
+
+    btn = Button(window, text="Dodaj", command=add)
+    btn.pack()
+
+    btn2 = Button(window, text="Wyczyść", command=clear)
+    btn2.pack()
+    
+    lbl2 = Label(window, text=" ")
+    lbl2.pack()
+
+
+    
 lbl = Label(window, text="Witaj w schowku!")
-lbl.grid(column=0,row=0)
+lbl.pack()
 
-txt = Entry(window,width=50)
-txt.grid(column=1, row=0)
+txt = Entry(window, width=50)
+txt.pack()
 
 btn = Button(window, text="Dodaj", command=add)
-btn.grid(column=2, row=0)
+btn.pack()
 
+btn2 = Button(window, text="Wyczyść", command=clear)
+btn2.pack()
 
-
+lbl2 = Label(window, text=" ")
+lbl2.pack()
 
 window.mainloop()
